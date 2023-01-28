39f17d35d4333beee401d3553c4834ff43f0756d
webmagiconline1 <78255428+webmagiconline1@users.noreply.github.com>
2023-01-28 16:24:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,17 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+variable "aws_access_key" {
+  default = "${var.aws_access_key}"
+}
+
+variable "aws_secret_key" {
+  default = "${var.aws_secret_key}"
+}
+
+variable "aws_region" {
+  default = "${var.aws_region}"
 }
